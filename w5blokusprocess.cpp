1ed7979c0aaef2006ff07c8c39fee7bb6d60d910
AtlasUnion <zruan@bu.edu>
2017-03-13 18:26:08 -0400

    Update w5blokusprocess.cpp

--- w5blokusprocess.cpp
+++ w5blokusprocess.cpp
@@ -8,76 +8,106 @@ using std::string;
 using std::vector;
 
 bool validLineInput(string test) {
-    for (char a: string test) {
+    for (char a: test) {
         if (a != '*' && a!='.') {
             return false;
         }
     }
     return true;
 }
+
+bool empty(string test) {
+  for (char a: test) {
+      if (a == '*') {
+          return false;
+      }
+  }
+  return true;
+}
 int main (int argc, char const *argv[]) {
     int textboxSize, count, countNum;
     string inputName;
     string outputName;
+    bool emp = true;
     count = 0;
     countNum = 0;
 
-    switch (argc)
-    {
-    case 1:
+    if (argc == 1){
         textboxSize = 5;
         string s;
-        while (cin >> s) {
-
+        vector < vector < string >> input;
+        while (getline(cin, s)) {
+          input.push_back(vector<string> ({s}));
          }
-        break;
-    case 2:
+       }
+    else if (argc == 2){
         textboxSize = std::stoi(argv[1]);
-        break;
-    case 3:
-        break;
-    default:
-        ifstream inFile;
-        ifstream outFile;
+      }
+
+    else if (argc == 3){
+    }
+    else {
+        std::ifstream inFile;
+        std::ofstream outFile;
+        string line;
         vector < vector < string >> input;
         textboxSize = std::stoi(argv[1]);
         inputName = argv[2];
         outputName = argv[3];
         inFile.open(inputName);
         outFile.open(outputName);
-        while(getline(inFile,line)) {
-            if (!validLineInput(line)) {
-                inFile.close();
-                outFile.close();
+        while(getline(inFile,line)) { //read the file into 2D vector
+          input.push_back(vector<string>({line}));
+        }
+        inFile.close();
+        for (int i = 0; i < input.size(); i++) {
+            if (input.size() < textboxSize) { //if incorrect input size
+              cout << "I am 1" << "\n";
+              return 2;
+            }
+          for (int j = 0; j < input[i].size(); j++) {
+            if (count == textboxSize && input[i][j] != "") { //no empty line
+              cout << count << "\n";
+              cout << i << "\n";
+              cout << "I am 2" << "\n";
+                return 2;
+            }
+            else if (input[i][j] == "" ) { //incorrect size
+              if(count != textboxSize) {
+                cout << count << "\n";
+                cout << i << "\n";
+                cout << input[i][j] << "\n";
+                cout << "I am 3" << "\n";
+                return 2;
+            }
+              count = 0; //if correct size
+              continue;
+            }
+            else if (!validLineInput(input[i][j])) { //contain other char
+              cout << "I am 4" << "\n";
                 return 1;
             }
-          input.push_back(vector<string>(line));
-    }
-    for (int i = 0; i < input.size(); i++) {
-        if(input[i].size() != textboxSize) {
-          return 2;
-        }
-      for (int j = 0; j < input[i].size(); j++) {
-        if (input[i][j] == '*') {
-            countNum++;
-        }
-        if (input[i][j] = '') {
-          if(count != textboxSize) {
-            return 2;
+            else if (input[i][j].length() != textboxSize) { //incorrect size
+              cout << "I am 5" << "\n";
+                return 2;
+            }
+            else if (count == textboxSize && emp == true) {
+              cout << "I am 6" << "\n";
+              return 3;
+            }
+            else if (!empty(input[i][j])) {
+              emp = false;
+            }
+            count++;
           }
-          if (countNum = 0) {
-            return 3;
+        }
+        for (int i = 0; i < input.size(); i++) {
+          for (int j = 0; j < input[i].size(); j++) {
+            outFile << input[i][j] << "\n";
           }
-          count = 0;
-          countNum = 0;
         }
-        else if (count == textboxSize && input[i][j] != '') {
-          return 2;
-        }
-        count++;
+        outFile.close();
       }
-    }
-        break;
-    }
+
     return 0;
 }
