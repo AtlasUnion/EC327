56cf68082a0a331148df8eff29680b40cdae0ea5
AtlasUnion <zruan@bu.edu>
2017-03-21 18:50:29 -0400

    Update w6blokusident.cpp

--- w6blokusident.cpp
+++ w6blokusident.cpp
@@ -4,7 +4,8 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
-
+#define COL 5
+#define ROW 5
 using std::vector;
 using std::make_pair;
 
@@ -13,16 +14,72 @@ typedef std::pair<int, int> CellType;
 typedef vector<CellType> PieceType;
 
 const int DefaultTSize = 5;
-
+// int COL;
 // Error codes
 const int BADCHAR = 1;
 const int BADSIZE = 2;
 const int NOTILE = 3;
 
-bool isBlank(String test) {
+// A function to check if a given cell (row, col) can be included in DFS
+int isSafe(int M[][COL], int row, int col, bool visited[][COL])
+{
+    // row number is in range, column number is in range and value is 1
+    // and not yet visited
+    return (row >= 0) && (row < ROW) &&
+           (col >= 0) && (col < COL) &&
+           (M[row][col] && !visited[row][col]);
+}
+
+// A utility function to do DFS for a 2D boolean matrix. It only considers
+// the 8 neighbours as adjacent vertices
+void DFS(int M[][COL], int row, int col, bool visited[][COL])
+{
+    // These arrays are used to get row and column numbers of 8 neighbours
+    // of a given cell
+    static int rowNbr[] = {-1, -1, -1,  0, 0,  1, 1, 1};
+    static int colNbr[] = {-1,  0,  1, -1, 1, -1, 0, 1};
+
+    // Mark this cell as visited
+    visited[row][col] = true;
+
+    // Recur for all connected neighbours
+    for (int k = 0; k < 8; ++k)
+        if (isSafe(M, row + rowNbr[k], col + colNbr[k], visited) )
+            DFS(M, row + rowNbr[k], col + colNbr[k], visited);
+}
+
+// The main function that returns count of islands in a given boolean
+// 2D matrix
+int countIslands(int M[][COL])
+{
+    // Make a bool array to mark visited cells.
+    // Initially all cells are unvisited
+    bool visited[ROW][COL];
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            visited[i][j] = 0;
+        }
+    }
+    // memset(visited, 0, sizeof(visited));
+
+    // Initialize count as 0 and travese through the all cells of
+    // given matrix
+    int count = 0;
+    for (int i = 0; i < ROW; ++i)
+        for (int j = 0; j < COL; ++j)
+            if (M[i][j] && !visited[i][j]) // If a cell with value 1 is not
+            {                         // visited yet, then new island found
+                DFS(M, i, j, visited);     // Visit all cells in this island.
+                ++count;                   // and increment island count
+            }
+
+    return count;
+}
+
+bool isBlank(std::string test) {
   for (int i = 0; i < test.size(); i++) {
     if (test[i] == '*') {
-      return false
+      return false;
     }
   }
   return true;
@@ -40,7 +97,7 @@ void rotate(std::vector<std::string> &test) {
   test = mirror;
 }
 
-void mirror(std::vector<std::string> &test) { //have not test for MxN matrix
+void mirror(std::vector<std::string> &test) {
   int ii, jj;
   std::vector<std::string> mirror1 = test;
   for(int i=0; i<test.size(); i++)
@@ -75,17 +132,43 @@ void removeBlankLine(vector<std::string> &test) {
   }
 }
 
-bool checkUnique(PieceType p1, PieceType p2, ,int TSize, bool[] &uniqueMatrix) {
+bool containOnlyOne(PieceType p, int TSize) {
+  int a[5][5] = {{0}};
+  for (auto const & loc : p){
+    a[loc.first][loc.second] = 1;
+}
+  if (countIslands(a) > 1) {
+    return false;
+  }
+  return true;
+}
+
+bool checkUnique(PieceType p1, PieceType p2 ,int TSize) {
     std::string base(TSize, '.');
     vector<std::string> compare1(TSize, base);
     vector<std::string> compare2(TSize, base);
-    for (auto const & loc : p){
+    for (auto const & loc : p1){
       compare1[loc.first][loc.second] = '*';
-      compare2[loc.first][loc.second] = '*';
+  }
+    for (auto const & loc : p2){
+    compare2[loc.first][loc.second] = '*';
   }
   removeBlankLine(compare1);
   removeBlankLine(compare2);
-  
+  for (int i=0; i < 4; i++) {
+    rotate(compare1);
+    if (compare1 == compare2) {
+      return false;
+    }
+  }
+  mirror(compare1);
+  for (int i=0; i < 4; i++) {
+    rotate(compare1);
+    if (compare1 == compare2) {
+      return false;
+    }
+  }
+  return true;
 }
 
 void print_piece(PieceType p, std::ostream *out, int TSize) {
@@ -168,7 +251,8 @@ int main(int argc, char const *argv[]) {
   std::ifstream infile;
   std::ofstream outfile;
   TSize = (argc == 1) ? DefaultTSize : std::stoi(argv[1]);
-
+  // COL = TSize;
+  vector<PieceType> result;
   if (argc > 2) {
     infile.open(argv[2]);
     in = &infile;
@@ -184,8 +268,26 @@ int main(int argc, char const *argv[]) {
   if (errorcode)
     return errorcode;
 
-  for (auto const & e : p)
-    print_piece(e, out, TSize);
+  for (auto const & e : p){
+    if(containOnlyOne(e,TSize)) {
+      result.push_back(e);
+    }
+  }
+  bool unique[p.size()] = {{1}};
+  for (int i = 0; i < p.size(); i++) {
+    for (int j = i+1; j < p.size(); j++) {
+      if (!checkUnique(p.at(i),p.at(j), int TSize)){
+        unique[j] = false;
+      }
+    }
+  }
+  for (int i = 0; i < p.size(); i++) {
+    if (unique[i] == true) {
+      print_piece(e, out, TSize);
+    }
+  }
+  // for (auto const & e : p)
+  //   print_piece(e, out, TSize);
 
   return 0;
 }
